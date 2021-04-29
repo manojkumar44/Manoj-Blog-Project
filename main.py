fdbbb599fca1a2049826dbd822127e300b3f76ba
MrKumar <mkumar.4478@gmail.com>
2021-04-29 20:14:56 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
 
+MY_SECRET_KEY = "n/5X.q[^Vk£G_}a}8:c@-2ke~dH*3"
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "n/5X.q[^Vk£G_}a}8:c@-2ke~dH*3"
+app.config['SECRET_KEY'] = MY_SECRET_KEY
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
